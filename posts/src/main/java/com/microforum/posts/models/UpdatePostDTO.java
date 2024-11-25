4e88d25dd6894a65f78017c4ae296c2de289d7b4
feehaam <mdfeeham@gmail.com>
2024-11-25 23:59:01 +0600

    Added more validation in post DTO's

--- posts/src/main/java/com/microforum/posts/models/UpdatePostDTO.java
+++ posts/src/main/java/com/microforum/posts/models/UpdatePostDTO.java
@@ -2,6 +2,7 @@ package com.microforum.posts.models;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 public class UpdatePostDTO {
     @NotEmpty(message = "Title cannot be empty")
     @Schema(description = "Title of the post", example = "Top 10 places to explore in Island")
+    @Size(min = 5, max = 100, message = "Title must be between 5 and 100 characters")
     private String title;
 
     @NotEmpty(message = "Content cannot be empty")
@@ -21,6 +23,7 @@ public class UpdatePostDTO {
             Exploring the Island was the best trip of my life.\s
             I had a chance to visit the beaches, the mountains, the cities and the villages.\s
             I met the locals and they were...""")
+    @Size(min = 10, max = 30000, message = "Content must be between 10 and 30000 characters")
     private String content;
 
     @NotEmpty(message = "Category cannot be empty")
@@ -32,5 +35,6 @@ public class UpdatePostDTO {
 
     @NotEmpty(message = "Tags cannot be empty")
     @Schema(description = "Tags of the post", example = "[Adventure, Travel, Island]")
+    @Size(min = 1, max = 5, message = "At least one tag is required, and no more than 5 tags are allowed")
     private List<String> tags;
 }
