d563ff25d6fc654587755c408734bf2e6780d30d
ShartazFeeham <mdfeeham@gmail.com>
2024-12-07 03:13:24 +0600

    Custom route prefix implemented

--- gateway/src/main/java/com/microforum/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/microforum/gateway/GatewayApplication.java
@@ -3,6 +3,9 @@ package com.microforum.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,17 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator microForumCustomRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/micro-forum/posts/**")
+						.filters(f -> f.rewritePath("/micro-forum/posts/(?<segment>.*)","/${segment}"))
+						.uri("lb://posts"))
+				// Add more route for other services
+				.build();
 	}
 
 }
