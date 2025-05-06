6845ab4b71b444df24af606a077c38eafa1fcd1c
ShartazFeeham <mdfeeham@gmail.com>
2025-05-06 20:39:46 +0600

    [Review service] application layer - mapper classes added

---
+++ review/src/main/java/com/open/forum/review/application/mapper/CommentMapper.java
@@ -0,0 +1,68 @@
+package com.open.forum.review.application.mapper;
+
+import com.open.forum.review.application.dto.comment.CommentCreateDTO;
+import com.open.forum.review.application.dto.comment.CommentReadDTO;
+import com.open.forum.review.application.dto.comment.CommentUpdateDTO;
+import com.open.forum.review.domain.model.comment.Comment;
+import com.open.forum.review.domain.model.comment.CommentContent;
+import com.open.forum.review.domain.model.comment.CommentStatus;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Mapper class for converting between Comment-related DTOs and the Comment entity.
+ * This class provides methods to convert CommentCreateDTO, CommentUpdateDTO, and CommentReadDTO to and from the Comment entity.
+ */
+public class CommentMapper {
+
+    /**
+     * Converts a CommentCreateDTO to a Comment entity.
+     *
+     * @param dto the CommentCreateDTO to convert
+     * @return the converted Comment entity
+     */
+    public static Comment toComment(CommentCreateDTO dto) {
+        final var builder = Comment.builder()
+                .commentId(dto.getCommentId())
+                .status(CommentStatus.PENDING)
+                .createdAt(ZonedDateTime.now())
+                .userId(dto.getUserId())
+                .isReply(dto.isReply())
+                .contents(new ArrayList<>(List.of(CommentContent.builder().content(dto.getContent()).build())));
+        if (dto.isReply()) {
+            builder.parentCommentId(dto.getParentCommentId());
+        } else {
+            builder.postId(dto.getPostId());
+        }
+        return builder.build();
+    }
+
+    /**
+     * Converts a CommentUpdateDTO to a Comment entity.
+     *
+     * @param dto the CommentUpdateDTO to convert
+     * @param comment the existing Comment entity to update
+     * @return the updated Comment entity
+     */
+    public static Comment toComment(CommentUpdateDTO dto, Comment comment) {
+        comment.getContents().add(CommentContent.builder()
+                .content(dto.getContent())
+                .commentId(dto.getCommentId())
+                .createdAt(ZonedDateTime.now())
+                .build());
+        return comment;
+    }
+
+    /**
+     * Converts a Comment entity to a CommentReadDTO.
+     *
+     * @param comment the Comment entity to convert
+     * @return the converted CommentReadDTO
+     */
+    public static CommentReadDTO toCommentReadDTO(Comment comment) {
+        return CommentReadDTO.from(comment);
+    }
+
+}
---
+++ review/src/main/java/com/open/forum/review/application/mapper/ReactionMapper.java
@@ -0,0 +1,55 @@
+package com.open.forum.review.application.mapper;
+
+import com.open.forum.review.application.dto.reaction.ReactionCreateDTO;
+import com.open.forum.review.application.dto.reaction.ReactionReadDTO;
+import com.open.forum.review.application.dto.reaction.ReactionUpdateDTO;
+import com.open.forum.review.domain.model.reaction.Reaction;
+import com.open.forum.review.domain.model.reaction.ReactionStatus;
+
+import java.time.ZonedDateTime;
+
+/*
+ * This class is responsible for mapping between different data transfer objects (DTOs) and domain models related to reactions.
+ * It provides methods to convert between ReactionCreateDTO and ReactionReadDTO, as well as to map a list of ReactionReadDTOs.
+ */
+public class ReactionMapper {
+
+    /**
+     * Converts a ReactionCreateDTO to a ReactionReadDTO.
+     *
+     * @param dto the ReactionCreateDTO to convert
+     * @return the converted ReactionReadDTO
+     */
+    public static Reaction toReaction(ReactionCreateDTO dto) {
+        return Reaction.builder()
+                .postId(dto.getPostId())
+                .commentId(dto.getCommentId())
+                .userId(dto.getUserId())
+                .reactionStatus(ReactionStatus.PENDING)
+                .reactionType(dto.getReactionType())
+                .createdAt(ZonedDateTime.now())
+                .build();
+    }
+
+    /**
+     * Converts a ReactionUpdateDTO to a Reaction entity.
+     *
+     * @param dto the ReactionUpdateDTO to convert
+     * @param reaction the existing Reaction entity to update
+     * @return the updated Reaction entity
+     */
+    public static Reaction toReaction(ReactionUpdateDTO dto, Reaction reaction) {
+        reaction.setReactionType(dto.getReactionType());
+        return reaction;
+    }
+
+    /**
+     * Converts a Reaction entity to a ReactionReadDTO.
+     *
+     * @param reaction the Reaction entity to convert
+     * @return the converted ReactionReadDTO
+     */
+    public static ReactionReadDTO toReactionReadDTO(Reaction reaction) {
+        return ReactionReadDTO.from(reaction);
+    }
+}
