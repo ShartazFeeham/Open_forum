5736e6a5942ce61706f7d8ce7804af0c5a57c45d
ShartazFeeham <mdfeeham@gmail.com>
2025-05-21 00:50:27 +0600

    [Review service] Infrastructure layer - Comment and reaction event publishers implemented

--- review/src/main/java/com/open/forum/review/infrastructure/events/ReactionEventPublisherImpl.java
+++ review/src/main/java/com/open/forum/review/infrastructure/events/ReactionEventPublisherImpl.java
@@ -1,16 +1,44 @@
 package com.open.forum.review.infrastructure.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.open.forum.review.domain.events.publisher.ReactionEventPublisher;
 import com.open.forum.review.domain.events.reaction.ReactionEvent;
+import com.open.forum.review.infrastructure.events.core.AbstractProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ReactionEventPublisherImpl implements ReactionEventPublisher {
+public class ReactionEventPublisherImpl extends AbstractProducer<String, String, ReactionEvent> implements ReactionEventPublisher {
+
+    private final ObjectMapper objectMapper;
+    private static final Logger log = LoggerFactory.getLogger(ReactionEventPublisherImpl.class);
+
+    protected ReactionEventPublisherImpl(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+        super(kafkaTemplate);
+        this.objectMapper = objectMapper;
+    }
+
     /**
      * @param event the event to be published
      */
     @Override
     public void publish(ReactionEvent event) {
+        this.send(event, (sendResult) -> {
+            log.info("Reaction event sent successfully: {}", sendResult);
+        }, (throwable) -> {
+            log.error("Reaction error sending event!", throwable);
+        });
+    }
 
+    @Override
+    public String getKey(ReactionEvent event) {
+        return getTopic(event) + event.getEventData().getReactionId();
+    }
+
+    @Override
+    public String getValue(ReactionEvent event) throws Exception {
+        return objectMapper.writeValueAsString(event);
     }
 }
